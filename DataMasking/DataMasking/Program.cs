b9df57f10ee6ace47603a7fdb9b7e7ca94cfdac9
sedat kocaer <sedatkocaer3734@hotmail.com>
2021-07-03 15:31:32 +0300

    Modify main program

--- DataMasking/DataMasking/Program.cs
+++ DataMasking/DataMasking/Program.cs
@@ -8,34 +8,20 @@ namespace DataMasking
         {
 
             #region String Mask
-            Console.WriteLine("Name =>");
-            var nameValue = Console.ReadLine();
-
-            Console.WriteLine("Surname =>");
-            var surnameValue = Console.ReadLine();
-
-            var nameMask = StringDataMask.CreateNewMask(nameValue);
-            var surNameMask = StringDataMask.CreateNewMask(surnameValue);
-            Console.WriteLine("Masking Value =>" + " " + nameMask + " " + surNameMask);
+            var nameMask = StringDataMask.CreateNewMask("Sedat");
+            var surNameMask = StringDataMask.CreateNewMask("Kocaer");
+            Console.WriteLine("Masking name Value =>" + " " + nameMask + " " + surNameMask);
             #endregion
 
 
             #region Date Masking
-            Console.WriteLine("Day =>");
-            var dayValue = Console.ReadLine();
 
-            Console.WriteLine("Month =>");
-            var monthValue = Console.ReadLine();
-
-            Console.WriteLine("Year =>");
-            var yearValue = Console.ReadLine();
-
-            var dateResponse = DateTimeMask.CreateNewMask(new DateTime(int.Parse(yearValue), int.Parse(monthValue), int.Parse(dayValue)));
+            var dateResponse = DateTimeMask.CreateNewMask(new DateTime(1997, 06, 06));
             Console.WriteLine("Masking Date Value =>" + " " + dateResponse);
             #endregion
 
-
-            Console.Write(CreditCardMasking.CreateNewMask("1234-5678-9101-1121"));
+            var creditCardResponse = CreditCardMasking.CreateNewMask("1234-5678-9101-1121");
+            Console.Write("Masking Credit Card Value =>" + " " + creditCardResponse);
         }
     }
 }
